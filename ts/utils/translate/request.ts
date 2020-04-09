e7fe3de73b780378b351806d6a3e021ca6ff381d
Eric Konks <exdevour@gmail.com>
2020-04-09 21:38:19 +0300

    Add try catch block for request.translate

--- ts/utils/translate/request.ts
+++ ts/utils/translate/request.ts
@@ -4,10 +4,17 @@ import * as T from './types'
 import * as C from './constants'
 
 export async function translate(params: T.ITranslateParams): Promise<T.ITranslateRequestAnswer> {
-    let response = await fetch(encodeURI(`${C.TRANSLATE_URL}?key=${C.TRANSLATE_API_KEY}&text=${params.text}&lang=${params.lang}`))
-    .then((response: { json: () => any; }) => response.json());
-    return {
-        text: response.text,
-        status: response.code,
-    };
+    try {
+        let response = await fetch(encodeURI(`${C.TRANSLATE_URL}?key=${C.TRANSLATE_API_KEY}&text=${params.text}&lang=${params.lang}`))
+        .then((response: { json: () => any; }) => response.json());
+        return {
+            text: response.text,
+            status: response.code,
+        };
+    } catch (err) {
+        return {
+            text: '',
+            status: err.message,
+        }
+    }
 }
