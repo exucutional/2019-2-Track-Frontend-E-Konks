4d65bb439a453bf57110812c740a1d992c3d7dc5
Eric Konks <exdevour@gmail.com>
2020-04-27 02:07:24 +0300

    Travis fixes

--- ts2/src/App.test.tsx
+++ ts2/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/FIFA 19 complete player dataset/i);
   expect(linkElement).toBeInTheDocument();
 });
